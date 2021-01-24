9ff2d00c507cac9ca6034704a73b7861ca60f084
ELYAZID-ISMAILI <elyazid.ismaili@usmba.ac.ma>
2021-01-24 15:35:04 +0100

    mise2

--- resources/views/store/index.blade.php
+++ resources/views/store/index.blade.php
@@ -23,24 +23,26 @@
 
                         @foreach($products as $product)
                         <!-- product -->
-                        <div class="col-md-3" id="backgr">
+                    
+                        <div  class="col-sm-6 col-md-4 col-lg-3" id="backgr">
+                            <img  src="uploads/products/{{$product->id}}/{{$product->image_name}}" alt="" style="width:100%; height:100%;" >
                             <div class="product">
                                 <div class="product-img">
-                                    <img src="uploads/products/{{$product->id}}/{{$product->image_name}}" alt="">
+                                    
                                     <div class="product-label">
-                                        <span class="sale">Offer!!</span>
+                                        <span class="sale" style="color:rgb(172, 10, 10);font-weight:bold">Offer!!</span>
                                     </div>
                                 </div>
                                 <div class="product-body">
-                                    <p class="product-category">{{$product->category->name}}</p>
-                                   // <h3 class="product-name"><a href="{{route('user.view',['id'=>$product->id])}}">{{$product->name}}</a></h3>
-                                    <h4 class="product-price"> {{$product->discount}} DH <del class="product-old-price"> {{$product->price}} DH </del></h4>
+                                    <p style="color:rgb(48, 59, 216);font-weight:bold" class="product-category">{{$product->category->name}}</p>
+                                    <h3 class="product-name"><a href="{{route('user.view',['id'=>$product->id])}}">{{$product->name}}</a></h3>
+                                    <h4 style="color:rgb(3, 100, 245);font-weight:bold;font-size:15px;"> {{$product->discount}} DH <del style="color:rgb(150, 8, 8);"> {{$product->price}} DH </del></h4>
                                     <div class="product-rating">
                                     </div>
                                     
                                 </div>
                                 <div class="add-to-cart">
-                               //     <a class="add-to-cart-btn" href="{{route('user.view',['id'=>$product->id])}}"><i class="fa fa-shopping-cart"></i>Purchase</a>
+                                   <a class="" href="{{route('user.view',['id'=>$product->id])}}"><i class="fa fa-shopping-cart"></i>Purchase</a>
                                 </div>
                             </div>
                         </div>
