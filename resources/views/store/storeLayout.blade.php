9bb526eac950354162cc1d9e819e9b6e189d1870
ELYAZID-ISMAILI <elyazid.ismaili@usmba.ac.ma>
2021-02-07 11:11:56 +0100

    msj

--- resources/views/store/storeLayout.blade.php
+++ resources/views/store/storeLayout.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     <link rel="stylesheet" href="{{mix('/css/theme.css')}}">
+    <link rel="stylesheet" href="{{mix('/css/app.css')}}">
     <title>Web site E-Commerce</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -18,92 +18,77 @@
     <!-- HEADER -->
     <header>
         <!-- TOP HEADER -->
-        <nav class="navbar navbar-expand  navbar-dark bg-dark" role="navigation">
-            <div class="container-fluid">
-                <div class="container">
-                    <ul class="navbar-nav me-auto mb-0 mb-sm-0">
-                        <li class="nav-item active"><a href="#"><i class="fa fa-phone"></i> +070-60-91-99</a></li><br>
-                        <li class="nav-item active"><a href="#"><i class="fa fa-envelope-o"></i> support@electro.com</a></li><br>
-                        <li class="nav-item active"><a href="#"><i class="fa fa-map-marker"></i> Meknes, Zitoune 11</a></li><br>
-                    </ul>
-                    <ul class="navbar-nav me-auto mb-2 mb-sm-0">
-                        @if(session()->has('user'))
-                          <li class="nav-item active"><a style="color:white" href="{{route('user.history')}}">{{session()->get('user')->full_name}} </a></li>  
-                          <li class="nav-item active"><a href="{{route('user.logout')}}"><i class="fa fa-user-o"></i> Logout</a></li>
-                        @else
-                        <li class="nav-item active"><a href="{{route('user.login')}}"><i class="fa fa-user-o"></i> Login</a></li>
+        <nav class="navbar navbar-expand-lg  navbar-dark" role="navigation" id="top-header">
+            
+                <div class="container-fluid " class="col-12">
+                            <div class="col-lg-8">
+                                <ul class="navbar-nav me-auto mb-0 mb-sm-0">
+                                    <li class="nav-item active" class="col-md-2" ><a href="#"><i class="fa fa-phone"></i> +070-60-91-99</a></li>
+                                    <li class="nav-item active" class="col-md-2"><a href="#"><i class="fa fa-envelope-o"></i> support@electro.com</a></li>
+                                    <li class="nav-item active" class="col-md-2"><a href="#"><i class="fa fa-map-marker"></i> Meknes, Zitoune 11</a></li>
+                                </ul>
+                            </div>
+                            
                         
-                        <li class="nav-item active"><a href="{{route('user.signup')}}"><i class="fa fa-user-o"></i> SignUp</a></li>
-                        @endif
-                        
-                    </ul>
+                        <div class="col-lg-4">
+                            <ul class="navbar-nav me-auto mb-2 mb-sm-0">
+                                @if(session()->has('user'))
+                                  <li class="nav-item active" class="col-md-2"><a style="color:white" href="{{route('user.history')}}">{{session()->get('user')->full_name}} </a></li>  
+                                  <li class="nav-item active" class="col-md-2"><a href="{{route('user.logout')}}"><i class="fa fa-user-o"></i> Logout</a></li>
+                                @else
+                                <li class="nav-item active" class="col-md-2"><a href="{{route('user.login')}}"><i class="fa fa-user-o"></i> Login</a></li>
+                                
+                                <li class="nav-item active" class="col-md-2"><a href="{{route('user.signup')}}"><i class="fa fa-user-o"></i> SignUp</a></li>
+                                @endif
+                                
+                            </ul>
+                        </div>
                 </div>
-            </div>
+            
         </nav>
         <!-- /TOP HEADER -->
 
         <!-- MAIN HEADER -->
-        <nav class="navbar navbar-expand navbar-dark bg-info">
-            <div id="header">
+        <nav class="navbar bg-dark">
+           
                 <!-- container -->
-                <div class="navbar-nav me-auto mb-2 mb-sm-0">
+                <div class="container-fluid w-100">
                     <!-- row -->
-                    <div class="row">
+                   
                         <!-- LOGO -->
-                        <div class="col-md-3">
-                            <div class="header-logo">
-                                <a class="navbar-brand" href="#"><img src="img/youcan.png" alt=""></a>
-                                <a href="{{route('user.home')}}" class="logo">
-                                   
-                                </a>
-                            </div>
-                        </div>
-                        <!-- /LOGO -->
-    
-                        <!-- SEARCH BAR -->
-                        <div class="col-md-3 ">
-                            <div class="header-search" class="navbar-center">
-                                <form action="{{route('user.search')}}" method="get">
-                                    <div class="custom_search_top" >
-                                        <input class="input" style="border-radius: 40px 0px 0px 40px;" name="n" placeholder="Search here">
-                                        <button  class="search-btn" class="btn btn-outline-success my-2 my-sm-0">Search</button>
-                                    </div>
-                                </form>
-                            </div>
-                        </div>
-                        <!-- /SEARCH BAR -->
-    
-                        <!-- ACCOUNT -->
-                        <div class="navbar-nav ml-auto" >
-                            <div class="header-ctn">
-                                <!-- Cart -->
-                                <div  class="dropdown" class="navbar-nav">
-                                    <a class="dropdown-toggle " class="text-sm-left" id="custom_shopping_cart" href="{{route('user.cart')}}">
-                                        <i class="fa fa-shopping-cart"></i>
-                                        <span>Your Cart</span>
-                                    </a>
-    
+                            <div class="w-25">
+                                <div class="">
+                                    <a class="" href="{{route('user.home')}}"><img src="img/youcan.png" alt=""></a>
                                 </div>
-                                <!-- /Cart -->
-    
-                                <!-- Menu Toogle -->
-                                
-                                <!-- /Menu Toogle -->
                             </div>
-                        </div>
-                        <!-- /ACCOUNT -->
-                    </div>
-                    <!-- row -->
-                </div>
-                <!-- container -->
-            </div>
+                            
+                            <div class="w-50">
+                                    <form action="{{route('user.search')}}" method="get">
+                                        <div class="input-group">
+                                            <input type="search" class="form-control" placeholder="Search..." style="border-radius: 40px 0px 0px 40px;max-width: 500px;">
+                                            <span class="input-group-btn">
+                                            <button class="btn btn-success" type="submit">Search</button>
+                                            </span>
+                                       </div>
+                                    </form>
+                            </div>
+                            <div class="w-10"></div>
+                            <div class="w-15 h-100" >
+                                    <div  class="navbar-nav">
+                                        
+                                        <a class="dropdown-toggle " id="custom_shopping_cart" href="{{route('user.cart')}}">
+                                            <i class="fa fa-shopping-cart"></i>
+                                            <span>Your Cart</span>
+                                        </a>
+                                    </div>
+                            </div>
+                        
+                </div>            
         </nav>
-        <!-- /MAIN HEADER -->
     </header>
-    <!-- /HEADER -->
 
     <!-- NAVIGATION -->
-    <nav class="navbar navbar-expand-sm navbar-dark bg-info">
+    <nav class="navbar navbar-expand-sm  ml-auto" style="background-color:#00EADF">
         <!-- container -->
         <div class="container">
             <!-- responsive-nav -->
@@ -190,17 +175,21 @@
             </div>
             </div>
         </div>
-        <ul class="newsletter-follow" class="bi bi-align-center">
-            <li>
-                <a href="#"><i class="fa fa-facebook"></i></a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-            </li>
-            <li>
-                <a href="#"><i class="fa fa-instagram"></i></a>
-            </li>
-        </ul>
+        <div class="w-35"></div>
+        <div class="w-30">
+            <ul class="newsletter-follow" class="">
+                <li>
+                    <a href="#"><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a href="#"><i class="fa fa-instagram"></i></a>
+                </li>
+            </ul>
+        </div>
+        <div class="w-35"></div>
         </div>
         </div>
         </section>
@@ -215,62 +204,68 @@
     <!-- /NEWSLETTER -->
 
     <!-- FOOTER -->
-    <footer id="footer" >
+    <footer class="navbar bg-dark">
         <!-- top footer -->
-        <div class="section">
+        <div class="section w-100">
             <!-- container -->
-            <div class="container">
+            <div class="container w-100" >
                 <!-- row -->
-                <div class="row" >
-                    <div class="col-md-3 col-xs-6" >
-                        <div class="footer" >
-                            <h3 class="footer-title">About Us</h3>
-                            <p>Site For Electronic Devices and Components
-                            </p>
+                <div class="row w-100" >
+                    
+                        <div class="container-fluid " class="col-12 w-100" >
+                            <div class="w-25 h-100" >
+                                <div class="footer w-100" >
+                                    <h3 class="footer-title w-50">About Us</h3>
+                                    <p class="w-75">Site For Electronic Devices and Components
+                                    </p>
+                                    
+                                </div>
+                            </div>
+        
+                            <div class="w-25">
+                                <div class="footer">
+                                    <h3 class="footer-title">Categories</h3>
+                                    <ul class="footer-links">
+                                        <li><a href="#">Hot deals</a></li>
+                                        <li><a href="#">Laptops</a></li>
+                                        <li><a href="#">Smartphones</a></li>
+                                        <li><a href="#">Cameras</a></li>
+                                        <li><a href="#">Accessories</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+        
                             
+        
+                            <div class="w-25">
+                                <div class="footer">
+                                    <h3 class="footer-title">Information</h3>
+                                    <ul class="footer-links">
+                                        <li><a href="#">About Us</a></li>
+                                        <li><a href="#">Contact Us</a></li>
+                                        <li><a href="#">Privacy Policy</a></li>
+                                        <li><a href="#">Orders and Returns</a></li>
+                                        <li><a href="#">Terms & Conditions</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+        
+                            <div class="w-25">
+                                <div class="footer">
+                                    <h3 class="footer-title">Service</h3>
+                                    <ul class="footer-links">
+                                        <li><a href="#">My Account</a></li>
+                                        <li><a href="#">View Cart</a></li>
+                                        <li><a href="#">Wishlist</a></li>
+                                        <li><a href="#">Track My Order</a></li>
+                                        <li><a href="#">Help</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            
+                             
                         </div>
-                    </div>
-
-                    <div class="col-md-3 col-xs-3">
-                        <div class="footer">
-                            <h3 class="footer-title">Categories</h3>
-                            <ul class="footer-links">
-                                <li><a href="#">Hot deals</a></li>
-                                <li><a href="#">Laptops</a></li>
-                                <li><a href="#">Smartphones</a></li>
-                                <li><a href="#">Cameras</a></li>
-                                <li><a href="#">Accessories</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="clearfix visible-xs"></div>
-
-                    <div class="col-md-3 col-xs-6">
-                        <div class="footer">
-                            <h3 class="footer-title">Information</h3>
-                            <ul class="footer-links">
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                                <li><a href="#">Orders and Returns</a></li>
-                                <li><a href="#">Terms & Conditions</a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                    <div class="col-md-3 col-xs-6">
-                        <div class="footer">
-                            <h3 class="footer-title">Service</h3>
-                            <ul class="footer-links">
-                                <li><a href="#">My Account</a></li>
-                                <li><a href="#">View Cart</a></li>
-                                <li><a href="#">Wishlist</a></li>
-                                <li><a href="#">Track My Order</a></li>
-                                <li><a href="#">Help</a></li>
-                            </ul>
-                        </div>
-                    </div>
+                    
                 </div>
                 <!-- /row -->
             </div>
